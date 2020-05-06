85c9c3b5fec6c9dfdcd825f86c9913ce61fb95ee
Shayne Burgess <shayne.burgess@github.com>
2020-05-06 12:11:53 -0700

    Adding more brew stuff

--- brew.sh
+++ brew.sh
@@ -16,34 +16,17 @@ brew install hub
 brew cask install iterm2
 brew install zsh
 
+brew install azure-cli
+brew install helm
+brew install jq
+brew install watch
+brew install screen
+brew install ssh-copy-id
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
-
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
 
 # Install more recent versions of some macOS tools.
 brew install vim
